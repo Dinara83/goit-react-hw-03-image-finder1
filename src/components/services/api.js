9ef201df7f3b15f31310c9270d73a9e2eb4a46f7
Dinara83 <mavleeva.dinara@gmail.com>
2023-02-01 14:51:03 +0200

    api

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  pageURL: 'https://pixabay.com/api',
-  params: {
-    key: '31934563-bfdfc3e562fca017f9814bb5d',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    per_page: 12,
-  },
-});
+const BASE_URL = 'https://pixabay.com/api';
+const API_KYE = '31934563-bfdfc3e562fca017f9814bb5d';
 
-export const searchApi = async (q, page = 1) => {
-  const data = await instance.get('/', {
+axios.defaults.baseURL = BASE_URL;
+
+export const searchApi = (searchRequest, page = 1, per_page = 12) => {
+  return axios.get('/', {
     params: {
-      q,
+      q: searchRequest,
       page,
+      key: API_KYE,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page,
     },
   });
-  return data;
 };
